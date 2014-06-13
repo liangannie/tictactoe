92e581b4087e87f98d7e3708893adefeddb3a810
liangannie <liangannie@users.noreply.github.com>
2014-06-13 15:13:49 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+(function($) {
+ 
+    var player1 = ' ';
+    var player2 = ' ';
+    var endOfGame = false;
+    var playerTurn = 0;
+    
+    var boxes = $("button");
+    //var arr = $.makeArray(boxes);
+    //var click = $(this).data('click');
+    
+    //create boxes
+    for (var i = 0; i < 9; i++) {
+        $("#my-game").append("<button></button>");
+        
+        $("button").each(function(i) {
+            $(this).addClass('button-'+i);
+        });
+    };
+    
+    // if box is empty, set it to player 1, then switch turns
+     $("button").one('click', function () {
+         if (!!$("button").length) {     
+             if (playerTurn === 0) {
+                    $(this).append('x');
+                    playerTurn = 1;
+                } else {
+                    $(this).append('o');
+                    playerTurn = 0;
+                }
+             } 
+     });
+    
+})(jQuery);
