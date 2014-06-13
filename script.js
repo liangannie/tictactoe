f74daa7668b6b48f8e4fded4687336310f8d521e
liangannie <liangannie@users.noreply.github.com>
2014-06-13 16:46:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,22 @@
     var endOfGame = false;
     var playerTurn = 0;
     
-    var boxes = $("button");
+    var boxes = $('button');
     //var arr = $.makeArray(boxes);
     //var click = $(this).data('click');
     
     //create boxes
     for (var i = 0; i < 9; i++) {
-        $("#my-game").append("<button></button>");
+        $('#my-game').append('<button></button>');
         
-        $("button").each(function(i) {
+        $('button').each(function(i) {
             $(this).addClass('button-'+i);
         });
     };
     
     // if box is empty, set it to player 1, then switch turns
-     $("button").one('click', function () {
-         if (!!$("button").length) {     
+     $('button').one('click', function () {
+         if (!!$('button').length) {     
              if (playerTurn === 0) {
                     $(this).append('x');
                     playerTurn = 1;
