52964e8f8b0e2346cdb55eb2122886781becf0f8
liangannie <liangannie@users.noreply.github.com>
2014-12-22 21:44:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,19 @@
  
     var gameOver = false;
     var playerTurn = "X"; 
-    var boxesArr= null;
-    var boxes = $(".ttc");
-
     
     //create boxes
     for (var i = 0; i < 9; i++) {
-        $("#my-game").append("<div class='ttc'></div>");
+        $("#my-game").append("<div class='ttc button-" + i + "'></div>");
+    }
+  
+var boxesArr = $.makeArray($('.ttc'));
+  
+if (gameOver === false) {
+      $(".ttc").one('click', function () { 
         
-        $(".ttc").each(function(i) {
-            $(this).addClass('button-'+i);
-        });
-    };
-    
-    //creates array after boxes are created
-    var boxesArr = jQuery.makeArray($('.ttc')); 
-    
-    // if box is empty, set it to player 1, then switch turns
-    $(".ttc").one('click', function () {
-        
-        $(this).append('<p>' + playerTurn + '</p>');      
-        winnerCheck(boxesArr, playerTurn);
+          $(this).append('<p>' + playerTurn + '</p>');
+          winnerCheck(boxesArr, playerTurn);
         
             if (playerTurn === "X") {
                     playerTurn = "O";
@@ -30,9 +22,12 @@
                     playerTurn = "X";
                 }
      });
-    
+} else {
+  alert("test!!!");
+}
+  
     function winnerCheck(boxesArr, playerTurn) {
-        if ( ($(boxesArr[0]).text() == playerTurn && $(boxesArr[1]).text() == playerTurn && $(boxesArr[2]).text() == playerTurn) ||
+        if (($(boxesArr[0]).text() == playerTurn && $(boxesArr[1]).text() == playerTurn && $(boxesArr[2]).text() == playerTurn) ||
             ($(boxesArr[3]).text() == playerTurn && $(boxesArr[4]).text() == playerTurn && $(boxesArr[5]).text() == playerTurn) ||
             ($(boxesArr[6]).text() == playerTurn && $(boxesArr[7]).text() == playerTurn && $(boxesArr[8]).text() == playerTurn) ||
             ($(boxesArr[0]).text() == playerTurn && $(boxesArr[3]).text() == playerTurn && $(boxesArr[6]).text() == playerTurn) ||
@@ -41,16 +36,19 @@
             ($(boxesArr[0]).text() == playerTurn && $(boxesArr[4]).text() == playerTurn && $(boxesArr[8]).text() == playerTurn) ||
             ($(boxesArr[2]).text() == playerTurn && $(boxesArr[4]).text() == playerTurn && $(boxesArr[6]).text() == playerTurn) ){
             endOfGame();
-        };    
-    };
+          return;
+        }
+    }
 
-        function endOfGame() {
+    function endOfGame() {
          var button = $("#reload");
          $("#message").append("Player " + playerTurn + " won!");
-         button.show();
+         gameOver = true;
+         
+         button.show();   
          button.click(function() {
              location.reload();
          });
-    };
+    }
     
 })(jQuery);
